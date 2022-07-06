8db3c8752dd5e6b143206446646d2973f50dba75
Eduard6421 <eduard_poe2000@yahoo.com>
2022-07-06 03:04:29 +0300

    Compleded and tested models for movie API

--- backend/src/homeflix/movie/models.py
+++ backend/src/homeflix/movie/models.py
@@ -4,6 +4,11 @@ from django.contrib.auth import get_user_model
 from django.utils.translation import gettext_lazy as _
 from django.core.validators import MinLengthValidator
 
+CONTENT_TYPE = (
+    ('movie', "MOVIE"),
+    ('show', "SHOW")
+)
+
 
 class Movie(models.Model):
 
@@ -15,7 +20,71 @@ class Movie(models.Model):
     title = models.CharField(_('title'), max_length=30, blank=False,
                              null=False, validators=[MinLengthValidator(1)])
     created_by = models.ForeignKey(get_user_model(), on_delete=models.CASCADE,
-                                   related_name='created_by',
+                                   related_name='created_movies',
+                                   related_query_name='created_by',
                                    blank=False, null=False)
+    content_type = models.CharField(
+        max_length=5, choices=CONTENT_TYPE,
+        default='movie', blank=False, null=False)
     created_at = models.DateTimeField(_('created at'), auto_now_add=True)
     updated_at = models.DateTimeField(_('updated at'), auto_now=True)
+
+
+class Tag(models.Model):
+
+    class Meta:
+        verbose_name = 'tag'
+        verbose_name_plural = 'tags'
+
+    id = models.UUIDField(_('id'), default=uuid.uuid4, primary_key=True)
+    name = models.CharField(_('tag name'), blank=False,
+                            null=False, max_length=255)
+    movies = models.ManyToManyField(
+        Movie, related_name='tags', related_query_name='movie')
+    created_by = models.ForeignKey(get_user_model(), on_delete=models.CASCADE,
+                                   related_name='created_tags',
+                                   related_query_name='created_by',
+                                   blank=False, null=False)
+
+
+class Genre(models.Model):
+
+    class Meta:
+        verbose_name = 'genre'
+        verbose_name_plural = 'genres'
+
+    id = models.UUIDField(_('id'), default=uuid.uuid4, primary_key=True)
+    name = models.CharField(_('genre name'), blank=False,
+                            null=False, max_length=255)
+    movies = models.ManyToManyField(
+        Movie, related_name='genres', related_query_name='movie')
+    created_by = models.ForeignKey(get_user_model(), on_delete=models.CASCADE,
+                                   related_name='created_genres',
+                                   related_query_name='created_by',
+                                   blank=False, null=False)
+
+
+class Listing(models.Model):
+
+    class Meta:
+        verbose_name = 'listing'
+        verbose_name_plural = 'listings'
+
+    id = models.UUIDField(_('id'), default=uuid.uuid4, primary_key=True)
+
+    stream_url = models.URLField(_('stream url'), blank=False, null=False)
+    season_number = models.PositiveSmallIntegerField(
+        _('season number'), null=False, blank=False, default=0)
+    episode_number = models.PositiveSmallIntegerField(
+        _('episode number'), null=False, blank=False)
+    content_description = models.TextField(
+        _('content description'), null=False, blank=False, max_length=10000)
+
+    movie = models.ForeignKey(
+        Movie, on_delete=models.CASCADE, related_name='listings',
+        null=False, blank=False,)
+
+    created_by = models.ForeignKey(get_user_model(), on_delete=models.CASCADE,
+                                   related_name='created_listings',
+                                   related_query_name='created_by',
+                                   blank=False, null=False)
--- backend/src/homeflix/movie/tests/test_movies_model.py
+++ backend/src/homeflix/movie/tests/test_movies_model.py
@@ -4,7 +4,7 @@ Tests for the profile model
 
 from django.test import TestCase
 from django.contrib.auth import get_user_model
-from movie.models import Movie
+from movie.models import Movie, Tag, Genre, Listing
 
 
 def create_superuser(**params):
@@ -15,6 +15,10 @@ def create_superuser(**params):
 def create_user(**params):
     """Create and return a new user"""
     return get_user_model().objects.create_user(**params)
+
+
+def create_movie(**params):
+    return Movie.objects.create(**params)
 
 
 class MovieModelTests(TestCase):
@@ -38,3 +42,95 @@ class MovieModelTests(TestCase):
 
         self.assertEqual(movie.title, 'A Jolly good show')
         self.assertEqual(movie.created_by, user)
+
+
+class TagModelTests(TestCase):
+
+    def test_create_tag(self):
+
+        email = 'user@example.com'
+        password = 'my5trongrand0mp4ss'
+
+        user = create_superuser(email=email, password=password)
+
+        tag = Tag.objects.create(
+            name='#Winner',
+            created_by=user,
+        )
+
+        self.assertEqual(tag.name, '#Winner')
+        self.assertEqual(tag.created_by, user)
+
+        movie = Movie.objects.create(
+            title='A Jolly good show',
+            created_by=user,
+        )
+
+        movie.tags.add(tag)
+        movie.save()
+        movie.refresh_from_db()
+
+        self.assertEqual(movie.tags.count(), 1)
+        self.assertEqual(movie.tags.all()[0].name, '#Winner')
+
+
+class GenreModelTests(TestCase):
+
+    def test_create_genre(self):
+
+        email = 'user@example.com'
+        password = 'my5trongrand0mp4ss'
+
+        user = create_superuser(email=email, password=password)
+
+        genre = Genre.objects.create(
+            name='#Winner',
+            created_by=user,
+        )
+
+        self.assertEqual(genre.name, '#Winner')
+        self.assertEqual(genre.created_by, user)
+
+        movie = Movie.objects.create(
+            title='A Jolly good show',
+            created_by=user,
+        )
+
+        movie.genres.add(genre)
+        movie.save()
+        movie.refresh_from_db()
+
+        self.assertEqual(movie.genres.count(), 1)
+        self.assertEqual(movie.genres.all()[0].name, '#Winner')
+
+
+class ListingModelTests(TestCase):
+
+    def test_create_genre(self):
+
+        email = 'user@example.com'
+        password = 'my5trongrand0mp4ss'
+
+        user = create_superuser(email=email, password=password)
+
+        movie = Movie.objects.create(
+            title='A Jolly good show',
+            created_by=user,
+        )
+
+        listing = Listing.objects.create(
+            stream_url="https://example.com/",
+            season_number=0,
+            episode_number=1,
+            content_description='The first episode of the movie',
+            created_by=user,
+            movie=movie
+        )
+
+        self.assertEqual(listing.stream_url, "https://example.com/")
+        self.assertEqual(listing.season_number, 0)
+        self.assertEqual(listing.episode_number, 1)
+        self.assertEqual(listing.content_description,
+                         'The first episode of the movie')
+        self.assertEqual(listing.created_by, user)
+        self.assertEqual(listing.movie, movie)
